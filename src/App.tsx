b2bf091d1808047eba5ea32ba47f3017f9e373b2
Yosuke Sakamura <y-sakamura@digital-sphere.co.jp>
2019-03-13 16:18:36 +0900

    fix servicejs

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,38 @@ import './App.css';
 import SpreadSheet from './components/spreadsheet'
 import Header from './components/header'
 
-class App extends React.Component {
+interface Props {
+  text?: string
+}
+
+interface State {
+  objName: string
+}
+
+export interface SelectObjEvent extends React.FormEvent<HTMLInputElement> {
+  target: HTMLInputElement;
+}
+
+class App extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      objName: ''
+    }
+    this.handleObjChange = this.handleObjChange.bind(this)
+  }
+
+  handleObjChange(e: SelectObjEvent) {
+    this.setState({
+      objName: e.target.value
+    })
+  }
+
   public render() {
     return (
       <div className="App">
-        <Header />
+        <Header onchange={this.handleObjChange} />
         <SpreadSheet />
       </div>
     );
@@ -15,3 +42,28 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+  // handleObjChange(e: SelectObjEvent) {
+  //   console.log('call!!')
+  //   this.setState({
+  //     objName: e.target.value
+  //   })
+  //   const remoteController = new Service().getRemoting()
+  //   console.log(remoteController)
+  //   new Promise((resolve, reject) => {
+  //     console.log('promise')
+  //     remoteController['fetchAccounts'](['Account'], (res: any, evt: any) => {
+  //       console.log('remote!!')
+  //       if (evt.status) resolve(res)
+  //       else console.error('error:' + evt)
+  //     })
+  //   })
+  //   .then((res) => {console.log(res)})
+  // }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,30 @@
 import * as React from 'react'
+import { SelectObjEvent } from '../App'
 
-export default class Header extends React.Component {
+interface State {
+  text: string
+}
+
+interface Props {
+  onchange: (e: SelectObjEvent) => void
+}
+
+
+export default class Header extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props)
+  }
+
   render(): JSX.Element {
     return (
       <div>
         <select>
           <label>オブジェクト</label>
           <option value=''>---</option>
+          <option value='Account'>Account</option>
+          <option value='Contact'>Contact</option>
+          <option value='Opportunity'>Opportunity</option>
         </select>
       </div>
     )
